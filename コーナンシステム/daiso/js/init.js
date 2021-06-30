a193d2c84d0459bb256a8ff82883db5b562f4ddf
Kohe_Ioroi <ioroikouhei64@gmail.com>
2021-06-30 22:46:01 +0900

    DBを分離。

--- コーナンシステム/daiso/js/init.js
+++ コーナンシステム/daiso/js/init.js
@@ -1,3 +1,4 @@
+// Firebase ConfigをInitialize
 var firebaseConfig = {
 	apiKey: "AIzaSyDlqQrxpx5tXJkX_K33BPKbfHiOjFodbGE",
 	authDomain: "konan089-a83b7.firebaseapp.com",
@@ -8,15 +9,15 @@ var firebaseConfig = {
 	appId: "1:932034786555:web:91a52eede50a3d152aac5e",
 	measurementId: "G-NXD6GY7SK3"
 };
+// Firebase Appが過剰登録されていないか確認
 if (firebase.apps.length === 0) {
 	firebase.initializeApp(firebaseConfig);
 }
+//認証シーケンス
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
-firebase.auth().onAuthStateChanged(function (user)
-{
+firebase.auth().onAuthStateChanged(function (user) {
 	if (user) {
-		firebase.auth().currentUser.getIdToken(true).then(function (idToken)
-		{
+		firebase.auth().currentUser.getIdToken(true).then(function (idToken) {
 			userid = firebase.auth().currentUser.email;
 			if (userid != "konan089@handy.app") {
 				alert("不正なユーザーを検知しました。管理者に問い合わせてください。ログアウトを実行します。");
@@ -33,12 +34,10 @@ firebase.auth().onAuthStateChanged(function (user)
 		$(".hideme").css("visibility", "hidden");
 		var uiConfig = {
 			callbacks: {
-				signInSuccessWithAuthResult: function (authResult, redirectUrl)
-				{
+				signInSuccessWithAuthResult: function (authResult, redirectUrl) {
 					return true;
 				},
-				uiShown: function ()
-				{
+				uiShown: function () {
 				}
 			},
 			signInFlow: 'popup',
@@ -51,8 +50,8 @@ firebase.auth().onAuthStateChanged(function (user)
 		// firebase.auth().signInWithEmailAndPassword("konan089@handy.app", "konan089").then(() => { alert("現在BIG-Kシステムがダウンしているため、6/20までシステムを開放しています。"); location.reload(true); });
 	}
 });
-function getParam_init(name, url)
-{
+
+function getParam_init(name, url) {
 	if (!url) url = window.location.href;
 	name = name.replace(/[\[\]]/g, "\\$&");
 	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
@@ -61,11 +60,7 @@ function getParam_init(name, url)
 	if (!results[2]) return '';
 	return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
-if (location.search && getParam_init("forcereload")) {
-	location.reload(true);
-}
-function funcCompare(a, b)
-{
+function funcCompare(a, b) {
 	if (a["Daiban"] < b["Daiban"]) return -1;
 	if (a["Daiban"] > b["Daiban"]) return 1;
 	if (a["Tana"] < b["Tana"]) return -1;
@@ -74,54 +69,50 @@ function funcCompare(a, b)
 	if (a["Retu"] > b["Retu"]) return 1;
 	return 0;
 }
-function dataInsert(path, data)
-{
+//下記よりCRUD命令
+//指定したデータを自動パスで追加(C)
+function dataInsert(path, data) {
 	var dfd = $.Deferred();
 	firebase.database().ref(path).push().set(data).then(
-		() =>
-		{
+		() => {
 			dfd.resolve();
 		}
 	);
 	return dfd.promise();
 }
-function dataTargetInsert(path, target, data)
-{
+//指定したデータを指定パスで追加(C)
+function dataTargetInsert(path, target, data) {
 	var dfd = $.Deferred();
 	firebase.database().ref(path + "/" + target).set(data).then(
-		() =>
-		{
+		() => {
 			dfd.resolve();
 		}
 	);
 	return dfd.promise();
 }
-function dataGet(path, variable, data)
-{
+//指定したキーで検索して取得(R)
+function dataGet(path, variable, data) {
 	var dfd = $.Deferred();
 	firebase.database().ref(path).orderByChild(variable).equalTo(data).once("value").then(
-		(i) =>
-		{
+		(i) => {
 			if (i.exists()) { dfd.resolve(i.val()); } else { dfd.resolve({}); }
 		});
 	return dfd.promise();
 }
-function getMaster(path, key)
-{
+//指定したパスを取得(R)
+function getMaster(path, key) {
 	var dfd = $.Deferred();
 	firebase.database().ref(path).orderByChild(key).once("value").then(
-		(i) =>
-		{
+		(i) => {
 			if (i.exists()) { dfd.resolve(i.val()); } else { dfd.resolve({}); }
 		});
 	return dfd.promise();
 }
-function dataUpdate(path, key, data)
-{
+//指定したキーを指定データで上書き(U)
+function dataUpdate(path, key, data) {
 	var dfd = $.Deferred();
 	firebase.database().ref(path).child(key).get().then(
-		(snapshot) =>
-		{
+		(snapshot) => {
 			if (snapshot.exists()) {
 				dataDelete(path, key);
 				dataInsert(path, data);
@@ -133,42 +124,39 @@ function dataUpdate(path, key, data)
 	);
 	return dfd.promise();
 }
-function dataPatch(path, key, data)
-{
+//指定したキーを指定差分データで更新(U)
+function dataPatch(path, key, data) {
 	var dfd = $.Deferred();
 	firebase.database().ref(path).child(key).update(data).then(
-		() =>
-		{
+		() => {
 			dfd.resolve();
 		}
 	);
 	return dfd.promise();
 }
-function dataDelete(path, key)
-{
+//指定したキーを削除(D)
+function dataDelete(path, key) {
 	var dfd = $.Deferred();
 	firebase.database().ref(path + "/" + key).remove().then(
-		() =>
-		{
+		() => {
 			dfd.resolve();
 		}
 	);
 	return dfd.promise();
 }
-function TableGet(path)
-{
+//指定したパスのデータをすべて取得
+function TableGet(path) {
 	var dfd = $.Deferred();
 	firebase.database().ref(path).once("value").then(
-		(i) =>
-		{
+		(i) => {
 			if (i.exists()) { dfd.resolve(i.val()); } else { dfd.resolve({}); }
 		});
 	return dfd.promise();
 }
-window.onbeforeprint = () =>
-{
-	$("[name=JANCODE]").each((index, element) =>
-	{
+
+
+window.onbeforeprint = () => {
+	$("[name=JANCODE]").each((index, element) => {
 		if (String($(element).text()).length == 13) {
 			$(element).barcode(String($(element).text()), "ean13", { barWidth: 1, barHeight: 10 });
 		} else if (String($(element).text()).length == 8) {
@@ -176,13 +164,26 @@ window.onbeforeprint = () =>
 		}
 	});
 };
-function showPopup(msg)
-{
+
+//メッセージウインドウを作成。５秒後に自動で消滅する。
+function showPopup(msg) {
 	if (typeof popuptimer !== "undefined") {
 		clearTimeout(popuptimer);
 	}
 	$(".popup").text(msg);
 	$('.popup').addClass('js_active');
 	$('.popup').hover(() => { $('.popup').removeClass('js_active'); });
-	popuptimer = setTimeout(() => { $('.popup').removeClass('js_active'); }, 3000);
+	popuptimer = setTimeout(() => { $('.popup').removeClass('js_active'); }, 5000);
+}
+function showload() {
+	$("#message").text("処理中");
+	$("#main").fadeOut();
+	$("#load").fadeIn();
+}
+function showmain() {
+	$("#load").fadeOut();
+	$("#main").fadeIn();
+}
+function no_scroll(event) {
+	event.preventDefault();
 }
---
+++ コーナンシステム/daiso/js/master-editor.js
@@ -0,0 +1,155 @@
+﻿function init() {
+    showload();
+    $("#message").text("デバイスセットアップ...");
+    $("#jan_input").change(function () {
+        if ($("#jan_input").val().length) {
+            dbsearch($("#jan_input").val());
+        }
+    });
+    document.getElementById("jan_input").onkeydown = (e) => {
+        key = e.code || 0;
+        if (key == 13) {
+            if ($("#jan_input").val() && $("#itemName").val()) {
+                insertData();
+            } else {
+                alert("入力内容に不備があります。");
+            }
+        }
+
+    };
+    showmain();
+    $("#jan_input").focus();
+    if (location.search) {
+        try {
+            $("#jan_input").val(getParam('jan'));
+            dbsearch(getParam('jan'));
+        } catch {
+        }
+    }
+}
+
+function dbsearch(JAN) {
+    showload();
+    $("#message").text("データベース内を検索中");
+    $.when(
+        dataGet("Daiso_Master", "JAN", JAN),
+        dataGet("Daiso_HTData", "JAN", JAN)
+    ).done(
+        function (master, ht) {
+            var data = master;
+            var htdata = ht;
+            if (Object.keys(data).length) {
+                $("#itemName").val(data[Object.keys(data)[0]]["ItemName"]);
+                $("#price").val(data[Object.keys(data)[0]]["Price"]);
+                $(".filter-option").html(data[Object.keys(data)[0]]["Price"]);
+                if (data[Object.keys(data)[0]]["isFood"]) { document.getElementById("isFood").checked = true; };
+                doubledCheck(JAN);
+                $("#daibanTitle").css("background-color", "transparent");
+                $("#priceTitle").css("background-color", "transparent");
+                $("#isFoodTitle").css("background-color", "transparent");
+                showmain();
+                $('#Daiban').focus(() => { $(this).select(); });
+            } else if (Object.keys(htdata).length) {
+                $("#itemName").val(htdata[Object.keys(htdata)[0]]["ItemName"]);
+                $("#itemName").focus();
+                doubledCheck(JAN);
+                $("#daibanTitle").css("background-color", "#FFFF00");
+                $("#isFoodTitle").css("background-color", "#FFFF00");
+                $("#priceTitle").css("background-color", "#FFFF00");
+                showmain();
+                $('#Daiban').focus(() => { $(this).select(); });
+            } else {
+                showmain();
+                $('#itemName').focus();
+            }
+        }
+    );
+}
+
+function nextfeild(str) {
+    if (str.value.length >= str.maxLength) {
+        for (var i = 0, elm = str.form.elements; i < elm.length; i++) {
+            if (elm[i] == str) {
+                (elm[i + 1] || elm[0]).focus();
+                break;
+            }
+        }
+    }
+    return (str);
+}
+function insertData() {
+    itemName = document.getElementById("itemName").value;
+    JAN = document.getElementById("jan_input").value;
+    Price = document.getElementById("price").value;
+    isFood = document.getElementById("isFood").checked;
+    isDoubled = document.getElementById("isDoubled").checked;
+    var data = { "Bumon": "24", "ItemName": itemName, "JAN": JAN, "PB": "", "Price": Price, "isFood": isFood, "isDoubled": isDoubled };
+    if (itemName && JAN && Price) {
+        showload();
+        $.when(
+            dataGet("Daiso_Master", "JAN", JAN),
+            dataGet("Daiso", "JAN", JAN)
+        ).done(
+            (func1, func2) => {
+                if (Object.keys(func1).length == 0) {
+                    dataTargetInsert("Daiso_Master", JAN, data);
+                    showPopup("データ書き込み完了");
+                } else {
+                    if (confirm("データはすでに存在します。上書きして良いですか？")) {
+                        dataTargetInsert("Daiso_Master", JAN, data);
+                        Object.keys(func2).forEach((key) => {
+                            dataPatch("Daiso", key, data);
+                        })
+                        showPopup("データ上書き完了");
+                    } else {
+                        showPopup("中断しました。")
+                    }
+
+                }
+                showmain();
+            }
+        )
+    }
+};
+function resetData() {
+    document.getElementById("itemName").value = "";
+    document.getElementById("jan_input").value = "";
+    document.getElementById("isDoubled").checked = false;
+}
+function delData(dataid) {
+    var dfd = $.Deferred();
+    $.when(
+        dataDelete("Daiso", dataid)
+    ).done(
+        function () { dfd.resolve(); }
+    );
+    return dfd.promise();
+
+}
+
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+function doubledCheck(JAN) {
+    var dfd = $.Deferred();
+    $.when(
+        dataGet("Main", "JAN", JAN)
+    ).done(
+        function (data) {
+            if (Object.keys(data).length) {
+                document.getElementById("isDoubled").checked = true;
+            } else {
+                document.getElementById("isDoubled").checked = false;
+            }
+            showPopup("コーナン競合判定完了。結果:" + document.getElementById("isDoubled").checked)
+            dfd.resolve();
+        }
+    );
+    return dfd.promise();
+}
